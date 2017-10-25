4d792ff5412a5dd5ad60e5da6b5bb00a7bced910
James Chapman <jjrchapman92@gmail.com>
2017-10-24 18:28:15 -0600

    Implemented processRequest in requestProtocol

--- src/serverComponents/RequestProtocol.java
+++ src/serverComponents/RequestProtocol.java
@@ -1,7 +1,48 @@
 package serverComponents;
 
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+
+import customTypes.idImagePair;
+
 public class RequestProtocol {
-	public String processRequest(String input) {
-		return null;
+	/**
+	 * Reference to a collection of POUI's that can be accessed by a client through this protocol
+	 */
+	private LinkedList<idImagePair<String, LinkedList<ImageIcon>>> pouiCollection;
+
+	public RequestProtocol() {
+		pouiCollection = new LinkedList<idImagePair<String, LinkedList<ImageIcon>>>();
+		LinkedList<ImageIcon> images = new LinkedList<ImageIcon>();
+		for (int i = 1; i <= 3; i++) {
+			String fileName = "/Users/jameschapman/Projects/SED Projects/poui-server/Sample Images/" + "step" + Integer.toString(i) + ".jpg";
+			Image image;
+			try {
+				image = ImageIO.read(new File(fileName));
+				ImageIcon img = new ImageIcon(image);
+				images.add(img);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		idImagePair<String, LinkedList<ImageIcon>> newPair = new idImagePair<String, LinkedList<ImageIcon>>("test", images);
+		pouiCollection.add(newPair);
+	}
+
+
+	public LinkedList<ImageIcon> processRequest(String input) {
+			int offset = pouiCollection.indexOf(new idImagePair<String, LinkedList<ImageIcon>>("test", null));
+			if (offset == -1) {
+				return null;
+			}
+			else {
+				return pouiCollection.get(offset).getImages();
+			}
 	}
 }
