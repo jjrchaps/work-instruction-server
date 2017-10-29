2d2bf494382f71f6d1e24acc6b9eddc131733798
James Chapman <jjrchapman92@gmail.com>
2017-10-29 12:35:54 -0600

    Changed all uses of LinkedList<ImageIcon> to Images class

--- src/customTypes/idImagePair.java
+++ src/customTypes/idImagePair.java
@@ -1,9 +1,5 @@
 package customTypes;
 
-import java.util.LinkedList;
-
-import javax.swing.ImageIcon;
-
 /**
  * Stores an Event e and a string identifier in a pair to be stored
  * within the EventContainer class.
@@ -12,21 +8,21 @@ import javax.swing.ImageIcon;
 
 public class idImagePair<sedid, images> {
 		private String productID;
-		private LinkedList<ImageIcon> images;
+		private Images images;
 		
-		public idImagePair(String id, LinkedList<ImageIcon> images) {
+		public idImagePair(String id, Images images) {
 			this.productID = id;
 			this.images = images;
 		}
 		
 		public String getProductID() { return this.productID; }
-		public LinkedList<ImageIcon> getImages() { return this.images; }
+		public Images getImages() { return this.images; }
 		
 		@Override
 		public boolean equals(Object o) {
 			if (!(o instanceof idImagePair)) return false;
 			@SuppressWarnings("unchecked")
-			idImagePair<String, LinkedList<ImageIcon>> compareTo = (idImagePair<String, LinkedList<ImageIcon>>) o;
+			idImagePair<String, Images> compareTo = (idImagePair<String, Images>) o;
 			return this.productID.equals(compareTo.getProductID());
 		}
 }
