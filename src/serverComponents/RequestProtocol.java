a5eb3d8660d30b3990bbd96eee03d5a4a29867e6
James Chapman <jjrchapman92@gmail.com>
2017-10-29 17:26:12 -0600

    Fixed RequestProtocol to prevent null pointer exceptions

--- src/serverComponents/RequestProtocol.java
+++ src/serverComponents/RequestProtocol.java
@@ -2,16 +2,22 @@ package serverComponents;
 
 import containers.POUIContainer;
 import customTypes.Images;
+import customTypes.ServerPOUI;
 
 public class RequestProtocol {
 	private POUIContainer pouiContainer;
-	
+
 	public RequestProtocol(POUIContainer container) {
 		pouiContainer = container;
 	}
 
 
 	public Images processRequest(String input) {
-		return pouiContainer.getPOUI(input).getImages();
+		ServerPOUI poui = pouiContainer.getPOUI(input);
+		if (poui != null) {
+			return poui.getImages();
+		} else {
+			return null;
+		}
 	}
 }
