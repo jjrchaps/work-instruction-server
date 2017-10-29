355ae963b49730ba175bf3361613fabee24fdcc4
James Chapman <jjrchapman92@gmail.com>
2017-10-29 16:47:21 -0600

    Edited requestProtocol to properly use the POUIContainer to retrieve POUIs

--- src/serverComponents/RequestProtocol.java
+++ src/serverComponents/RequestProtocol.java
@@ -1,24 +1,17 @@
 package serverComponents;
 
+import containers.POUIContainer;
 import customTypes.Images;
-import customTypes.ServerPOUI;
 
 public class RequestProtocol {
-	/**
-	 * Reference to a collection of POUI's that can be accessed by a client through this protocol
-	 */
-//	private  pouiCollection;
-
-//	public RequestProtocol() {
-		//TODO: Implement usage of new container to store all POUI's. It should be created in Server.java and passed to all threads when they're created.
-//		pouiCollection = 
-//		Images images = new Images("/Users/jameschapman/Projects/SED Projects/poui-server/Sample Images/");
-//		ServerPOUI<String, Images> newPair = new ServerPOUI<String, Images>("test", images);
-//		pouiCollection.add(newPair);
-//	}
+	private POUIContainer pouiContainer;
+	
+	public RequestProtocol(POUIContainer container) {
+		pouiContainer = container;
+	}
 
 
 	public Images processRequest(String input) {
-		return null;
+		return pouiContainer.getPOUI(input).getImages();
 	}
 }
