2239d8106887efb207e9802fb09ca2b82f71b9d8
James Chapman <jjrchapman92@gmail.com>
2017-11-12 10:24:33 -0600

    Added timingStorer to storage package to facilitate thread safe storage of timing information

---
+++ src/storage/timingStorer.java
@@ -0,0 +1,92 @@
+package storage;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.Calendar;
+import java.util.concurrent.BlockingQueue;
+
+public class timingStorer extends Thread {
+	/**
+	 * A queue to pull timing information to store to file.
+	 */
+	private BlockingQueue<String> queue;
+
+	/**
+	 * Path to the folder containing all POUIs, as all timings will be stored in a hidden folder within
+	 * this parent folder.
+	 */
+	private String pathToParentFolder;
+
+	public timingStorer(BlockingQueue<String> queue, String pathToParentFolder) {
+		this.queue = queue;
+		this.pathToParentFolder = pathToParentFolder;
+	}
+
+	public void run() {
+		while (true) {
+			try {
+				reportTimings(queue.take());
+			} catch (InterruptedException e) {
+				// even if interrupted we require thread to continue working, so do nothing with
+				// this error and keep storing items in the queue.
+			}
+		}
+	}
+
+	/**
+	 * Accepts an array of strings that represent the recorded timings from a client upon
+	 * completion of the product.
+	 * @param splitInput All the timings, as string, separated by a colon.
+	 * @return True if the timings were successfully handled, false otherwise.
+	 */
+	private boolean reportTimings(String input) {
+		String[] splitInput = input.split(":");
+		// Get the productID from the received request.
+		String productID = splitInput[1];
+
+		// check to see if this product ID already has a folder. If not, we create one.
+		if (!(new File(this.pathToParentFolder + "/.timings/" + productID)).exists()) {
+			if (!(new File(pathToParentFolder + "/.timings/" + productID).mkdir())) {
+				System.out.println("Failed to create timings folder for " + productID);
+				return false;
+			}
+		}
+		else {
+			if (!(new File(pathToParentFolder + "/.timings/" + productID).isDirectory())) {
+				System.out.println("File with name timings found, but it's not a directory.");
+				return false;
+			}
+		}
+
+		// create the filename for the text file that will contain all the timings.
+		// file names are formatted by the date (all builds that took place on the same day
+		// will be in the same file). The date is represented by DAY.MONTH.YEAR without the forward
+		// slashes.
+		Calendar calendar = Calendar.getInstance();
+		String fileName = this.pathToParentFolder + ".timings/" + productID + "/" + calendar.get(Calendar.DAY_OF_MONTH) + "." + 
+				(calendar.get(Calendar.MONTH) + 1) + "." + calendar.get(Calendar.YEAR) + ".txt";
+
+		// create the file to be written, and then write to it
+		try {
+			// check to see if this file has been created already, and if so, add a newline between these timings
+			// and previous timings
+			boolean newFile = !(new File(fileName).exists());
+			File file = new File(fileName);
+			PrintWriter out = new PrintWriter(new FileOutputStream(file, true));
+			if (!newFile) {
+				out.write("\n");
+			}
+			for (int i = 2; i < splitInput.length; i++) {
+				out.println("Step" + (i - 1) + ":" + splitInput[i]);
+			}
+			out.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("Failed to create file for product " + productID);
+			//TODO: Add custom exception
+		}
+
+		return true;
+	}
+}
