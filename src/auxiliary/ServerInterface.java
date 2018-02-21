02140adc092c1ee6198aadcffed36f524e48f11e
James Chapman <jjrchapman92@gmail.com>
2018-02-20 20:11:52 -0600

    Added new server interface class to auxiliary package

---
+++ src/auxiliary/ServerInterface.java
@@ -0,0 +1,18 @@
+package auxiliary;
+
+/**
+ * Server interface is the class that handles user interactions. The
+ * interface will be text based and will be started after the server's
+ * initial startup.
+ * @author jameschapman
+ */
+public class ServerInterface {
+	/**
+	 * Start is the function that will begin the ServerInterface
+	 * listening for interactions from the user. It will
+	 * receive/parse input and also control server components.
+	 */
+	public void start() {
+		
+	}
+}
